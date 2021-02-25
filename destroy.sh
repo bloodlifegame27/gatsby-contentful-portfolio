6a29889a76ecf8f92fede9e73954c5110a0d0258
dimon12091 <dmytro12091@gmail.com>
2021-02-26 00:52:58 +0200

    update files and add new token API

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,3 @@
-aws s3 rm s3://$(pulumi stack output | grep bucket_name | cut -b 18-)/ --recursive
+aws s3 rm s3://$(pulumi stack output bucket_name)/ --recursive
 pulumi destroy -y 
 pulumi stack rm $1  
--- update.sh
+++
@@ -1,2 +0,0 @@
-pulumi up -y
-aws s3 sync ./public s3://$(pulumi stack output | grep bucket_name | cut -b 18-)/ 
